ce30cdde807a7adf8145feb0d09694238d768609
Vinicius de Carvalho <vinyciux@gmail.com>
2021-08-30 18:39:22 -0300

    print best solutions

--- src/main/java/ie/ucd/cs/mas3/normsystem/main/cleanPf.java
+++ src/main/java/ie/ucd/cs/mas3/normsystem/main/cleanPf.java
@@ -1,6 +1,8 @@
 package ie.ucd.cs.mas3.normsystem.main;
 
 import java.io.FileNotFoundException;
+import java.text.DecimalFormat;
+import java.util.Arrays;
 import java.util.List;
 import org.uma.jmetal.util.archive.impl.NonDominatedSolutionListArchive;
 import org.uma.jmetal.util.fileoutput.SolutionListOutput;
@@ -23,17 +25,40 @@ public class cleanPf {
         Front pfFront = new ArrayFront(pfKnown);
 
         String dir = pfKnown + ".Clean";
+        int m=0;
         
         List<PointSolution> pflist = FrontUtils.convertFrontToSolutionList(pfFront);
         NonDominatedSolutionListArchive nd = new NonDominatedSolutionListArchive();
         for (PointSolution sol : pflist) {
             calculateHypervolumeForENGNottsAlgs.revertToMaximization(sol, sol.getNumberOfObjectives());
             nd.add(sol);
+            m=sol.getNumberOfObjectives();
         }
         pflist = nd.getSolutionList();
         for (PointSolution sol : pflist) {
             calculateHypervolumeForENGNottsAlgs.revertToMaximization(sol, sol.getNumberOfObjectives());
         }
+        PointSolution[] bests=new PointSolution[m];
+        double[] bestValues=new double[m];
+        for (PointSolution sol : pflist) {
+            for (int i = 0; i < m; i++) {
+                if(sol.getObjective(i) > bestValues[i]){
+                    bestValues[i]=sol.getObjective(i);
+                    bests[i]=sol;
+                }
+            }
+        }
+        DecimalFormat nf = new DecimalFormat("###.########");
+        System.out.println(Arrays.toString(bestValues));
+        for (int i = 0; i < m; i++) {
+            String sep="[";
+            for (int k = 0; k < m; k++) {
+                System.out.print(sep+nf.format(bests[i].getObjective(k)));
+                sep=", "; 
+            }
+            System.out.print("]");
+        }
+        
         new SolutionListOutput(pflist)
                 .setSeparator("\t")
                 .setFunFileOutputContext(new DefaultFileOutputContext(dir))
